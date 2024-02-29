ddfa7a83385b7df623d4bdd66eeafab833afacd5
jiyeon-dev <happysophiapark@gmail.com>
2024-03-01 03:29:51 +0900

    feat: add swagger

---
+++ backend/file-driver/file-driver-api/src/main/java/org/example/filedriverapi/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package org.example.filedriverapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger Config
+ * URL : /swagger-ui/index.html
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("File Driver API")
+                .description("File Driver Web Service REST API")
+                .version("1.0,0");
+    }
+
+}
