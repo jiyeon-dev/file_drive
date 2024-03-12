3ee451d4c5c1632db722a90eecd453e023a630d1
jiyeon-dev <happysophiapark@gmail.com>
2024-03-13 02:48:09 +0900

    feat: 폴더 리스트 조회 방식을 tanstack으로 변경

--- frontend/file-drive/src/components/FolderCardContainer.tsx
+++ frontend/file-drive/src/components/FolderCardContainer.tsx
@@ -1,19 +1,25 @@
-import { Folder } from "@/types";
+import { Folder, Response } from "@/types";
 import FolderCard from "./FolderCard";
 import { Link, useLoaderData, useSearchParams } from "react-router-dom";
 import { Button } from "./ui/button";
 import { ArrowLeftCircleIcon } from "lucide-react";
 import { memo } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { authFetch } from "@/lib/authFetch";
 
 export function FolderCardContainer() {
   const [searchParams] = useSearchParams();
-  const { folder, folders } = useLoaderData() as {
+  const { folder } = useLoaderData() as {
     folder: Folder;
-    folders: Folder[];
   };
   const folderId = (searchParams.get("folderId") || 1) as number;
   const hasParentFolder = folderId > 1;
   const parentId = folder?.parent?.id || 1;
+
+  const { data } = useQuery({
+    queryKey: ["folder", { folderId }],
+    queryFn: ({ queryKey }) => fetchFolders({ queryKey }),
+  });
 
   return (
     <>
@@ -28,7 +34,7 @@ export function FolderCardContainer() {
       </div>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2'>
-        {folders.map((folder) => (
+        {data?.map((folder) => (
           <FolderCard key={folder.id} folder={folder} />
         ))}
       </div>
@@ -37,3 +43,21 @@ export function FolderCardContainer() {
 }
 
 export const MemoizedFolderCardContainer = memo(FolderCardContainer);
+
+type SearchOption = { folderId: number };
+const fetchFolders = async ({
+  queryKey,
+}: {
+  queryKey: (string | SearchOption)[];
+}) => {
+  try {
+    const folderId = (queryKey[1] as SearchOption).folderId;
+    const response = await authFetch(`/api/folder/list?folderId=${folderId}`, {
+      method: "GET",
+    });
+    const result = (await response?.json()) as Response<Folder[]>;
+    return result.resultData;
+  } catch (e) {
+    return [];
+  }
+};
--- frontend/file-drive/src/page/Files.tsx
+++ frontend/file-drive/src/page/Files.tsx
@@ -24,18 +24,6 @@ const fetchFileTypes = async () => {
   }
 };
 
-const fetchFolders = async ({ folderId = 1 }) => {
-  try {
-    const response = await authFetch(`/api/folder/list?folderId=${folderId}`, {
-      method: "GET",
-    });
-    const result = (await response?.json()) as Response<Folder[]>;
-    return result.resultData;
-  } catch (e) {
-    return [];
-  }
-};
-
 const fetchFolder = async ({ folderId = 1 }) => {
   try {
     const response = await authFetch(`/api/folder?folderId=${folderId}`, {
@@ -53,8 +41,7 @@ export const loader = async ({ request }: { request: Request }) => {
     1) as number;
 
   const fileTypes = await fetchFileTypes();
-  const folders = await fetchFolders({ folderId });
   const folder = await fetchFolder({ folderId });
 
-  return defer({ fileTypes, folders, folder });
+  return defer({ fileTypes, folder });
 };
