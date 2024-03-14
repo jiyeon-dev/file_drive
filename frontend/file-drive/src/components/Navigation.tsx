52bc8444ea01df030beaf6d7e7b408cb8d644cc5
jiyeon-dev <happysophiapark@gmail.com>
2024-03-14 21:20:08 +0900

    feat: 네비게이션 바에 유저 정보 표시

--- frontend/file-drive/src/components/Navigation.tsx
+++ frontend/file-drive/src/components/Navigation.tsx
@@ -2,8 +2,18 @@ import { NavLink } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { FileIcon, StarIcon, TrashIcon } from "lucide-react";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { useQuery } from "@tanstack/react-query";
+import { authFetch } from "@/lib/authFetch";
+import { Member, Response } from "@/types";
 
 export default function Navigation() {
+  const { data } = useQuery({
+    queryKey: ["user"],
+    queryFn: fetchUser,
+    staleTime: 60 * 60 * 1000, // 1 시간
+    gcTime: 60 * 60 * 1000, // 1 시간
+  });
+
   return (
     <div className='fixed top-0 pt-24 bg-background flex flex-col items-center space-y-6 w-12 h-screen border-r'>
       <NavButton link='/files' title='전체 파일'>
@@ -17,7 +27,7 @@ export default function Navigation() {
       </NavButton>
 
       <Avatar className='absolute bottom-8'>
-        <AvatarFallback>CN</AvatarFallback>
+        <AvatarFallback>{data?.name.slice(0, 2).toUpperCase()}</AvatarFallback>
       </Avatar>
     </div>
   );
@@ -42,4 +52,14 @@ const NavButton = ({
       </NavLink>
     </Button>
   );
+};
+
+const fetchUser = async () => {
+  const response = await authFetch("/api/auth/user", {});
+  const result = (await response?.json()) as Response<Member>;
+  if (!result.resultStatus.isSuccess) {
+    throw new Error("유저를 찾을 수 없습니다.");
+  } else {
+    return result.resultData;
+  }
 };
